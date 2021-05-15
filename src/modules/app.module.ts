99ae5a00f64081757f869b27d6e3d40b12f20240
JackRKelly <kcjackkelly@gmail.com>
2021-05-14 22:06:36 -0500

    Added typeorm dep

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from '@controllers/app.controller';
 import { AppService } from '@services/app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      debug: false,
+      playground: true,
+      autoSchemaFile: 'schema.gql'
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'nestjs-gql',
+      entities: ['dist/**/*.model.js'],
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
